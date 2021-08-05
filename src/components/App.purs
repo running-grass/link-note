c1e9e4cbb6776e6462b4d5546bc8c83f73756560
Leo Liu <467195537@qq.com>
2021-08-05 23:46:45 +0800

    CU-akwnzj - 笔记可以原地编辑，失去焦点自动保存

--- src/components/App.purs
+++ src/components/App.purs
@@ -5,7 +5,7 @@ import Prelude
 
 import Control.Monad.Rec.Class (forever)
 import Control.Promise (Promise, toAffE)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe, isNothing)
 import Data.String.Regex (Regex, test, replace)
 import Data.String.Regex.Flags (global)
 import Data.String.Regex.Unsafe (unsafeRegex)
@@ -20,10 +20,11 @@ import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Subscription as HS
 import Html.Renderer.Halogen as RH
-import RxDB.RxCollection (bulkRemoveA, find, findOne, upsertA)
-import RxDB.RxDocument (isRxDocument, toJSON)
-import RxDB.RxQuery (emptyQueryObject, execA, primaryQuery)
+import RxDB.RxCollection (bulkRemoveA, find, upsertA)
+import RxDB.RxDocument (toJSON)
+import RxDB.RxQuery (emptyQueryObject, execA)
 import RxDB.Type (RxCollection, RxDocument)
+import Web.UIEvent.KeyboardEvent as KE
 
 
 type Input = { 
@@ -47,6 +48,7 @@ type File = Record (
 
 type State = { 
     currentId :: Maybe String,
+    currentNote :: Maybe String,
     note :: String, 
     coll :: RxCollection Note,
     collFile :: RxCollection File,
@@ -56,13 +58,15 @@ type State = {
     }
 
 data Action
-  = Submit 
+  = Submit String String 
   | SetNote String 
   | InitNote 
+  | New
+  | HandleKeyUp KE.KeyboardEvent
   | InitComp
   | SubmitIpfs String
   | Delete String 
-  | Edit String
+  | Edit String String
 
 
 
@@ -77,20 +81,32 @@ regFileLink = unsafeRegex "\\[\\[file-(.*?)\\]\\]" global
 getGatewayUriA :: IPFS -> Aff String
 getGatewayUriA ipfs = toAffE $ getGatewayUri ipfs 
 
+renderViewNote :: forall a . String -> Note -> HH.HTML a Action
+renderViewNote ipfsGatway note = if test regFileLink note.content 
+        then RH.render_ $ replace regFileLink ("<img src=\"" <> ipfsGatway <> "$1\">") note.content 
+        else  HH.text $ " " <> note.content
+
+renderEditNote :: forall a . Note -> HH.HTML a Action
+renderEditNote note = HH.div_ [
+  HH.textarea [ 
+    HP.placeholder "请在这里输入笔记内容！" 
+    , HP.rows 3
+    , HE.onValueInput \val -> Submit note.id val
+    , HE.onKeyUp \kbe -> HandleKeyUp kbe
+    , HP.value note.content 
+    ]
+  ]
 
 
-renderNote :: forall  a. String ->  Note  -> HH.HTML a Action
-renderNote ipfsGatway  note = 
+renderNote :: forall  a. String -> Maybe String ->  Note  -> HH.HTML a Action
+renderNote ipfsGatway currentId note = 
   HH.li_ 
     [
       HH.button [ HE.onClick \_ -> Delete note.id ] [ HH.text "删除" ]
-      , HH.button [ HE.onClick \_ -> Edit note.id ] [ HH.text "编辑" ]
-      , if test regFileLink note.content 
-        then RH.render_ $ replace regFileLink ("<img src=\"" <> ipfsGatway <> "$1\">") note.content 
-        -- then HH.text $ replace regFileLink ("<img src=\"" <> ipfsGatway <> "$&\">") note.content 
-        --  HH.img [ HP.src $ ipfsGatway <> note.content,
-        -- HP.width 200]
-        else  HH.text $ " " <> note.content
+      , HH.button [ HE.onClick \_ -> Edit note.id note.content ] [ HH.text "编辑" ]
+      , case currentId of
+        Nothing -> renderViewNote ipfsGatway note
+        Just id -> if id == note.id then renderEditNote note  else renderViewNote ipfsGatway note
     ]
 
 
@@ -99,16 +115,7 @@ render :: forall cs m. State -> H.ComponentHTML Action cs m
 render state =
   HH.div_
     [ 
-      HH.div_ [ 
-        HH.textarea 
-          [ HP.placeholder "请在这里输入笔记内容！" 
-            , HP.rows 5
-            , HE.onValueInput \val -> SetNote val
-            , HP.value state.note ]
-          ]
-    -- , HH.button [ HE.onClick \_ -> InitNote ] [ HH.text "加载" ]
-    , HH.button [ HE.onClick \_ -> Submit ] [ HH.text "保存" ]
-    , HH.ul_ $ state.noteList <#> renderNote (fromMaybe "ipfs://" state.ipfsGatway)
+    HH.ul_ $ state.noteList <#> renderNote (fromMaybe "ipfs://" state.ipfsGatway) state.currentId
     ]
     
 
@@ -120,30 +127,41 @@ toNote d = unsafePerformEffect $ toJSON d
  
 handleAction :: forall cs o m . MonadAff m =>  Action → H.HalogenM State Action cs o m Unit
 handleAction = case _ of
+  New -> do
+    coll <- H.gets _.coll
+    uuid <- H.liftEffect UUID.genUUID
+    let noteId = "note-" <> UUID.toString uuid
+    void $ H.liftAff $ upsertA coll { content: "",  id: noteId, type: "text"  }
+    H.modify_  _ { currentId = Just noteId, currentNote = Just "" }
   SetNote note -> do
     H.modify_ _ { note = note }
-  Submit ->  do 
-    note <- H.gets _.note
+  Submit noteId note->  do
     coll <- H.gets _.coll
-    currentId <- H.gets _.currentId
-    uuid <- H.liftEffect UUID.genUUID
-    let noteId = fromMaybe (UUID.toString uuid) currentId
-    void $ H.liftAff $ upsertA coll { content: note,  id: noteId, type: "text" }
-    H.modify_  _ { note = ""}
+    void $ H.liftAff $ upsertA coll { content: note,  id: noteId, type: "text"  }
+    H.modify_  _ { note = "" }
   SubmitIpfs path -> do
-    note <- H.gets _.note
     collFile <- H.gets _.collFile
+
     let fileId = "file-" <> path
     void $ H.liftAff $ upsertA collFile { cid: path,  id: fileId, mime: "", type: "" }
-    let newNote = note <> "[[" <> fileId <> "]]"
-    H.modify_  _ { note = newNote }
+    
+    note <- H.gets _.currentNote
+    id <- H.gets _.currentId
+
+    if isNothing id 
+    then pure unit 
+    else do
+      let note' = fromMaybe "" note
+      let id' = fromMaybe "" id 
+      let newNote = note' <> "[[" <> fileId <> "]]"
+      handleAction $ Submit id' newNote
+
   InitComp -> do
     ipfs <- H.gets _.ipfs
     _ <- H.subscribe =<< timer InitNote
     _ <- H.subscribe =<< subscriptPaste ipfs
     host <- H.liftAff $ getGatewayUriA ipfs
     H.modify_ _ { ipfsGatway = Just (host <> "/ipfs/") }
-    -- pure unit
   InitNote -> do
     coll <- H.gets _.coll
     query <-  H.liftEffect $ find coll emptyQueryObject
@@ -153,18 +171,18 @@ handleAction = case _ of
   Delete noteId -> do
     coll <- H.gets _.coll
     H.liftAff $ bulkRemoveA coll [noteId]
-  Edit noteId -> do
-    coll <- H.gets _.coll
-    q <- H.liftEffect $ findOne coll $ primaryQuery noteId
-    doc' <- H.liftAff $ execA q
-    isDoc <- H.liftEffect $ isRxDocument doc'
-    when isDoc do
-      note <- H.liftEffect $ toJSON doc'
-      H.modify_  _ { currentId = Just note.id, note = note.content }
+    H.modify_ _ { currentId = Nothing, currentNote = Nothing }
+  HandleKeyUp kbe 
+    | KE.key kbe == "Enter" -> handleAction New
+    | KE.key kbe == "Escape" -> H.modify_ _ { currentId = Nothing, currentNote = Nothing }
+    | otherwise -> pure unit
+  Edit noteId content -> do
+    H.modify_ _ { currentId = Just noteId, currentNote = Just content }
 
 initialState :: Input-> State
 initialState input = { 
   currentId: Nothing,
+  currentNote: Nothing,
   ipfsGatway: Nothing,
   note : "",
   coll : input.coll,

de4fa8838e6ca2440667312d02c8e31dd9a4a64b
Leo Liu <467195537@qq.com>
2021-12-28 17:31:02 +0800

    清除多余的代码

--- apps/notes/android/app/src/main/kotlin/com/example/notes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.notes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- apps/notes/lib/pages/topic_detail.dart
+++ apps/notes/lib/pages/topic_detail.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:models/models.dart';

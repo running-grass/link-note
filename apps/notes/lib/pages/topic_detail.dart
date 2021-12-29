4ab5d1dd5ade340dd66800783c583d1e7e2ce72e
Leo Liu <467195537@qq.com>
2021-12-29 23:05:52 +0800

    优化列表

--- apps/notes/lib/pages/topic_detail.dart
+++ apps/notes/lib/pages/topic_detail.dart
@@ -32,27 +32,24 @@ class _TopicState extends State<TopicDetail> {
           store.clearEditingNote();
         },
         child: Scaffold(
-            appBar: AppBar(
-                title: Observer(builder: (_) => Text(store.topicName)),
-                actions: [
-                  IconButton(
-                    onPressed: () {
-                      store.remove();
-                      Get.back();
-                    },
-                    icon: const Icon(Icons.delete_forever),
-                  ),
-                ]),
-            body: Row(
-              children: [
-                Observer(
-                    builder: (_) => Column(
-                          children: store.children
-                              .map((noteStore) => NoteItem(noteStore))
-                              .toList(),
-                        )),
-              ],
-            )));
+          appBar: AppBar(
+              title: Observer(builder: (_) => Text(store.topicName)),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    store.remove();
+                    Get.back();
+                  },
+                  icon: const Icon(Icons.delete_forever),
+                ),
+              ]),
+          body: Observer(
+              builder: (_) => ListView(
+                    children: store.children
+                        .map((noteStore) => NoteItem(noteStore))
+                        .toList(),
+                  )),
+        ));
     // });
   }
 }
@@ -79,22 +76,46 @@ class NoteItem extends StatelessWidget {
                 }
               }
             },
-            child: Container(
-                color: Colors.amber,
-                padding: const EdgeInsets.all(10),
-                width: 1000,
-                height: 50,
-                child: Observer(
-                    builder: (_) => store.status == NoteStatus.editing
-                        ? TextFormField(
-                            initialValue: store.content,
-                            onChanged: (val) {
-                              store.updateContent(val);
-                            },
-                            autofocus: true,
-                          )
-                        : Text(
-                            store.content,
-                          )))));
+            child: Observer(
+                builder: (_) => Container(
+                    color: Colors.amber,
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.circle_outlined,
+                              size: 16,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                                flex: 1,
+                                child: store.status == NoteStatus.editing
+                                    ? TextFormField(
+                                        initialValue: store.content,
+                                        onChanged: (val) {
+                                          store.updateContent(val);
+                                        },
+                                        autofocus: true,
+                                      )
+                                    : Text(
+                                        store.content,
+                                      )),
+                          ],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 80),
+                          child: Column(
+                            children: store.children
+                                .map((noteStore) => NoteItem(noteStore))
+                                .toList(),
+                          ),
+                        )
+                      ],
+                    )))));
   }
 }

3b8af2d2f7bb7af6d26faf9ffefc1f933020ae48
Leo Liu <467195537@qq.com>
2022-05-23 23:41:20 +0800

    修复搜索框不能退格的bug

--- packages/frontend/src/pages/topic/component/CardTree.tsx
+++ packages/frontend/src/pages/topic/component/CardTree.tsx
@@ -1,23 +1,28 @@
 import { observer } from "mobx-react";
-
+import { useCallback } from "react";
+import { useClickAway } from 'react-use'
 import { CardStore } from "../../../mobx/Card.store";
 import { useMultHotkeys, KEYMAP } from "../../../utils/hook";
 
-export const CardBody = observer(({ card }: { card: CardStore }) => {
-  const restorePositon = (target: HTMLInputElement | null) => {
-    if (!target) return;
-    card.belong.setNeedFocus({
-      card,
-      pos: target.selectionStart ?? undefined,
-    });
-  };
+const restorePositon = (card: CardStore, target: HTMLInputElement | null) => {
+  if (!target) return;
+  card.belong.setNeedFocus({
+    card,
+    pos: target.selectionStart ?? undefined,
+  });
+};
 
-  const ref = useMultHotkeys<HTMLInputElement>(
+export const CardEditInput = observer(({ card }: { card: CardStore }) => {
+  const ref: React.MutableRefObject<HTMLInputElement | null> = useMultHotkeys<HTMLInputElement>(
     {
-      [KEYMAP.NAV_PLANT_UP]: () => {
+      [KEYMAP.NAV_PLANT_UP]: (ev) => {
+        ev.preventDefault()
+
         card.navToPlantUp();
       },
-      [KEYMAP.NAV_PLANT_DOWN]: () => {
+      [KEYMAP.NAV_PLANT_DOWN]: (ev) => {
+        ev.preventDefault()
+
         card.navToPlantDown();
       },
       // [KEYMAP.NAV_LEFT]: () => {
@@ -27,6 +32,8 @@ export const CardBody = observer(({ card }: { card: CardStore }) => {
       //   console.log(card.id + "right");
       // },
       [KEYMAP.BACKSPACE]: (ev) => {
+        ev.preventDefault()
+
         const target = ev.target as HTMLInputElement;
 
         if (target.selectionStart === null) {
@@ -40,6 +47,8 @@ export const CardBody = observer(({ card }: { card: CardStore }) => {
         return true;
       },
       [KEYMAP.NEW_NEXT]: (ev, hv) => {
+        ev.preventDefault()
+
         const target = ev.target as HTMLInputElement;
         if (ref.current !== target) {
           return;
@@ -55,30 +64,41 @@ export const CardBody = observer(({ card }: { card: CardStore }) => {
           card.createNextCard(target.selectionStart, target.selectionEnd);
         }
       },
-      [KEYMAP.MOVE_UP]: () => {
+      [KEYMAP.MOVE_UP]: (ev) => {
+        ev.preventDefault()
+
         card.moveUp();
-        restorePositon(ref.current);
+        restorePositon(card, ref.current);
       },
-      [KEYMAP.MOVE_DOWN]: () => {
+      [KEYMAP.MOVE_DOWN]: (ev) => {
+        ev.preventDefault()
+
         card.moveDown();
-        restorePositon(ref.current);
+        restorePositon(card, ref.current);
       },
-      [KEYMAP.MOVE_LEFT]: () => {
+      [KEYMAP.MOVE_LEFT]: (ev) => {
+        ev.preventDefault()
         card.moveLevelUp();
-        restorePositon(ref.current);
+        restorePositon(card, ref.current);
       },
-      [KEYMAP.MOVE_RIGHT]: () => {
+      [KEYMAP.MOVE_RIGHT]: (ev) => {
+        ev.preventDefault()
         card.moveLevelDown();
-        restorePositon(ref.current);
+        restorePositon(card, ref.current);
       },
     },
     {
       enableOnTags: ["INPUT", "TEXTAREA"],
-      filterPreventDefault: false,
       enabled: true,
     },
     [card]
   );
+
+
+  useClickAway(ref, () => {
+    card.belong.setCurrentCard();
+    // alert('OUTSIDE CLICKED');
+  });
 
   const onContentChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     card.changeContent(ev.currentTarget.value);
@@ -93,7 +113,34 @@ export const CardBody = observer(({ card }: { card: CardStore }) => {
       onChange={onContentChange}
     />
   );
+})
+
+export const CardBody = observer(({ card }: { card: CardStore }) => {
+
+  const startEditing = useCallback(() => {
+    card.belong.setNeedFocus({
+      card,
+      pos: 10000
+    });
+    console.log('set current')
+  }, [card, card.belong])
+
+  console.log('card body', card.belong.currentEditingCard ,card.isEditing)
+  return (
+    card.isEditing ? <CardEditInput card={card} /> : <div
+      id={`card-header-${card.id}`}
+
+      className="card-header"
+      onClick={startEditing}
+    >
+
+      {card.content}
+    </div>
+
+  );
 });
+
+
 
 export const CardTree = observer(
   ({ cards }: { cards: CardStore[]; parent?: CardStore }) => {
@@ -114,3 +161,4 @@ export const CardTree = observer(
     );
   }
 );
+
